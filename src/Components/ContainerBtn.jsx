11ad0abb0def677988f60ec6a80148e0dd2284c1
Sawan jha <jhasawan02@gmail.com>
2026-02-02 21:05:40 +0530

    made sidebar styling and maincontainer

--- src/Components/ContainerBtn.jsx
+++ src/Components/ContainerBtn.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function ContainerBtn() {
   return (
-    <div className='border rounded-md'>ContainerBtn</div>
+    <div className='border rounded-md flex p-1 bg-gray-800'>
+        <button className='text-2xl  text-white w-auto  border-white p-0.5 border-2 rounded mr-6 max-h-fit '>All</button>
+        <button className='text-2xl  text-white w-auto  border-white p-0.5 border-2 rounded mr-6 max-h-fit'>Drama</button>
+        <button className='text-2xl  text-white w-auto  border-white p-0.5 border-2 rounded mr-6 max-h-fit'>Live</button>
+        <button className='text-2xl  text-white w-auto  border-white p-0.5 border-2 rounded mr-6 max-h-fit'>Mixes</button>
+        <button className='text-2xl  text-white w-auto  border-white p-0.5 border-2 rounded mr-6 max-h-fit'>Movies</button>
+        <button className='text-2xl  text-white w-auto  border-white p-0.5 border-2 rounded mr-6 max-h-fit'>Recently Uploaded</button>
+        <button className='text-2xl  text-white w-auto  border-white p-0.5 border-2 rounded mr-6 max-h-fit'>New to you</button>
+    </div>
+    {// will need to make dynamic buttons }
   )
 }
 
