9749422017b6cd3cf1b5d41e9231199a93a69a29
Sawan jha <jhasawan02@gmail.com>
2026-02-04 16:18:14 +0530

    env file made

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -8,5 +8,4 @@ export const user_avatr_icon = "https://encrypted-tbn0.gstatic.com/images?q=tbn:
 export const mic_icon_header = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWvkm-IIm70JONUoXGgJxI08erSIFmgtj_vA&s"
 export const search_icon_header = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf8E-ADHCUm0aVqpN5TmbJxdE1CLNw50o5JA&s"
 
-// api key
-export  const API_KEY = "AIzaSyAyb0tqpC50SYANlLvvi4OS2dzWvhYmGek"
+
